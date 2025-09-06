b7760fec15606a1ee3698720caf27e7ce38955f5
TinselMoon <vitorcolatusso@gmail.com>
2025-09-05 22:43:53 -0300

    solver

--- solver.c
+++ solver.c
@@ -2,8 +2,39 @@
 
 int compute_polish_expression (char *c) {
   Stack *s = create(strlen(c));
+  int result, a, b;
   for(int i = 0; c[i] != '\0'; i++){
+    if(c[i] >= '0' && c[i] <= '9'){
+      push(s, c[i]-48);
+    }
+    else{
+      switch (c[i]) {
+        case '+':
+          a = pop(s);
+          b = pop(s);
+          push(s, a+b);
+          break;
+        case '*':
+          a = pop(s);
+          b = pop(s);
+          push(s, a*b);
+          break;
+        case '-':
+          a = -pop(s);
+          b = pop(s);
+          push(s, a+b);
+          break;
+        case '/':
+          a = pop(s);
+          b = pop(s);
+          push(s, b/a);
+          break;
+      }
+    }
   }
+  result = pop(s);
+  destroy(s);
+  return result;
 }
 
 int main () {
